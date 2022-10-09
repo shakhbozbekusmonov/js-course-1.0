7788ea868e63cc93fdf64af05d42b52df8f15646
Shakhbozbek Usmonov <83509440+shakhbozbekusmonov@users.noreply.github.com>
2022-10-09 16:55:19 +0500

    Start

---
+++ app.js
@@ -0,0 +1,33 @@
+// Created by @miracleprogrammer "Shakhbozbek Usmonov"!
+// JS Course easy learning!
+
+// What is JavaScript?
+/* JavaScript was initially created to “make web pages alive”.
+ The programs in this language are called scripts.
+ They can be written right in a web page’s
+ HTML and run automatically as the page loads.
+ Scripts are provided and executed as plain text.
+ They don’t need special preparation or compilation to run.
+ In this aspect, JavaScript is very different from another language called Java.
+ */
+
+// JavaScript nima?
+/*    JavaScript dastlab "veb-sahifalarni jonli qilish" uchun yaratilgan.
+    Bu tildagi dasturlar skriptlar deb ataladi.
+    Ular to'g'ridan-to'g'ri veb-sahifaning HTML-da yozilishi va sahifa yuklanganda
+    avtomatik ravishda ishga tushishi mumkin.
+    Skriptlar oddiy matn sifatida taqdim etiladi va bajariladi.
+    Ularni ishga tushirish uchun maxsus tayyorgarlik yoki kompilyatsiya kerak emas.
+    Bu jihatdan JavaScript Java deb nomlangan boshqa tildan juda farq qiladi .
+*/
+
+// ----------------- Start -----------------
+alert("Hello JS!");
+/* alert() - Metodi browserni ochganingizda birinchi bo'lib yuqoridan
+ xabar ko'rinishida chiqib turadi, OK tugmasini bosangiz yo'qoladi.
+ Biror ogohlantirish va shunga o'xshagan xabarlarni chiqarishingiz mumkin!
+ */
+console.log("Hello world!");
+/* console.log() - browser console ga biror xabar chiqarishingiz mumkin,
+ JavaScriptda console eng yaxshi debuggur deyish mumkin 👌
+ */
